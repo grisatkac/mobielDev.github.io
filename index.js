c83a8cd7e62ad0c6c70d7b65a66db488d938f181
Grisha <grisatkac@mail.ru>
2019-10-17 02:21:03 +0300

    ch2

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const geoBtn = document.querySelector('#geo_btn');
-const vibBtn = document.querySelector('#vib_btn');
+const devPos = document.querySelector('#devicePos_btn');
 
-geoBtn.addEventListener('click', () => {
-    function havePosition(pos) {
-        console.log(`x: ${pos.coords.latitude}`);
-        console.log(`y: ${pos.coords.longitude}`);
-        alert(`x: ${pos.coords.latitude}`);
-        alert(`y: ${pos.coords.longitude}`);
-    }
 
-    function noPosition(pos) {
-        console.log("device don't have geolocation sensor");
-    }
-
-    let options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-    };
-
-    navigator.geolocation.getCurrentPosition(havePosition, noPosition, options);
+devPos.addEventListener('deviceorientation', (event) => {
+    /*console.log(`absolute: ${event.absolute}`);
+    console.log(`apha: ${event.alpha}`)*/
+    document.querySelector('#absolute').innerHTML = `absolute: ${event.absolute}`;
+    document.querySelector('#alpha').innerHTML = `alpha: ${event.alpha}`;
+    document.querySelector('#beta').innerHTML = `beta: ${event.beta}`;
+    document.querySelector('#gamma').innerHTML = `gamma: ${event.gamma}`;
 });
 
-vibBtn.addEventListener('click', () => {
-    navigator.vibrate(2000);
-})
+geoBtn.addEventListener('click', () => {
+    if("geolocation" in navigator) {
+        function havePosition(pos) {
+            console.log(`x: ${pos.coords.latitude}`);
+            console.log(`y: ${pos.coords.longitude}`);
+            alert(`x: ${pos.coords.latitude}`);
+            alert(`y: ${pos.coords.longitude}`);
+        }
+
+        function noPosition(pos) {
+            console.log("device don't have geolocation sensor");
+        }
+
+        let options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        navigator.geolocation.getCurrentPosition(havePosition, noPosition, options);
+    }else {
+        console.log('navigation in not avaible');
+    }
+});
+
+
+
+
