302e9d4d4e305d2caade4b380c4c926a0a84f2b5
Grisha <grisatkac@mail.ru>
2019-10-17 02:52:41 +0300

    ch6

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const geoBtn = document.querySelector('#geo_btn');
+const devPos = document.querySelector('#devPos');
 
+devPos.addEventListener('deviceorientation', (event) => {
+    /*console.log(`absolute: ${event.absolute}`);
+    console.log(`apha: ${event.alpha}`)*/
+    document.querySelector('#absolute').innerHTML = `absolute: ${event.absolute} \n`;
+    document.querySelector('#alpha').innerHTML = `alpha: ${event.alpha} \n`;
+    document.querySelector('#beta').innerHTML = `beta: ${event.beta} \n`;
+    document.querySelector('#gamma').innerHTML = `gamma: ${event.gamma} \n`;
+});
+
+geoBtn.addEventListener('click', () => {
+    if("geolocation" in navigator) {
+        function havePosition(pos) {
+            console.log(`x: ${pos.coords.latitude}`);
+            console.log(`y: ${pos.coords.longitude}`);
+            alert(`x: ${pos.coords.latitude}`);
+            alert(`y: ${pos.coords.longitude}`);
+        }
+
+        function noPosition(pos) {
+            console.log("device don't have geolocation sensor");
+        }
+
+        let options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        navigator.geolocation.getCurrentPosition(havePosition, noPosition, options);
+    }else {
+        console.log('navigation in not avaible');
+    }
+});
